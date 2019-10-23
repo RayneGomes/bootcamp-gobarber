6fa9ac2060518fa6909fa1f759318570aa5b12ec
raynegomes <raynegomes@gmail.com>
2019-10-23 17:48:40 -0300

    upload avatar de usuarios e agendamentos

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/mutter';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -14,5 +21,12 @@ routes.post('/users', UserController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;

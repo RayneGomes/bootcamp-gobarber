b07012b5cc1880c9bce88b9ce7d23a47228f23fe
raynegomes <raynegomes@gmail.com>
2019-10-24 13:43:23 -0300

    criacao de serviço de visualizacao de agendamentos do prestador logado

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,12 @@ import multer from 'multer';
 
 import multerConfig from './config/mutter';
 
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
+import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
-import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -26,6 +27,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 

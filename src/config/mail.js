1fd2e7129da23b53aaf78db4d14abdf6ea28f920
raynegomes <raynegomes@gmail.com>
2019-10-28 17:43:48 -0300

    envio de emails para notificar cancelamento de agendamentos

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '9153f920da527f',
+    pass: '604cb3230eec58',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
